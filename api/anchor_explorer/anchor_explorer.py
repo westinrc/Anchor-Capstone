3eeadf1e8792cf92988c3dbba7ad836047e7a5b4
Dhalton95 <dhaltonhuber@Dhaltons-MacBook-Pro.local>
2017-10-17 11:50:19 -0700

    Implemented functionality for editing settings from an endpoint

--- api/anchor_explorer/anchor_explorer.py
+++ api/anchor_explorer/anchor_explorer.py
@@ -4,13 +4,9 @@ from utils.random_patient_generation import generate
 from utils.settings_update import update_settings
 import sys
 import json
-import os
 
 app = Flask('anchor_explorer')
 redis = Redis(host='redis', port=6379)
-UPLOAD_FOLDER = '/tmp'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-APP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 @app.route('/example', methods=['GET'])
 def example():
@@ -25,24 +21,9 @@ def generate_random_patients():
 
 @app.route('/update-settings', methods=['PUT'])
 def settings_update():
-    #settings = request.data
-    #settings = update_settings(str(settings))
-    #return settings
-
-    target = os.path.join(APP_ROOT, 'settings')
-
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    
-
-    settings_file = request.files['file']
-    file_name = 'settings.xml'
-    destination = '/'.join([target, file_name])
-    settings_file.save(destination)
-    sys.stderr.write(target)
-    sys.stderr.write(destination)
-
-    return request.data
+    settings = request.data
+    settings = update_settings(str(settings))
+    return settings
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
