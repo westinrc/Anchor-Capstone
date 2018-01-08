8eb4f9b83b19fafb8678dd888a0cdb7a661e4423
Dhalton95 <dhaltonhuber@Dhaltons-MacBook-Pro.local>
2018-01-08 13:17:50 -0700

    Working on getting a connection to the database setup so I can write a script to read in patient data. Having trouble connecting.

--- api/anchor_explorer/anchor_explorer.py
+++ api/anchor_explorer/anchor_explorer.py
@@ -4,6 +4,7 @@ from utils.random_patient_generation import generate
 from utils.settings_update import update_settings
 import sys
 import json
+from mysql.connector import (connection)
 
 app = Flask('anchor_explorer')
 redis = Redis(host='redis', port=6379)
@@ -24,6 +25,13 @@ def settings_update():
     settings = request.data
     settings = update_settings(str(settings))
     return settings
+
+@app.route('/test-database', methods=['GET'])
+def test_db_connection():
+    cnx = connection.MySQLConnection(user='root', password='Capstone_Password',
+                              host='127.0.0.1',
+                              database='capstone_DB')
+    return "Success!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
