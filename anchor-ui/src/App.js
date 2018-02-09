02ad4579010d3bab60a86350fe4239d35c939000
Westin <westinrc@ymail.com>
2018-02-08 21:29:59 -0700

    CPST-5 :: Added the initial components for the UI.

--- anchor-ui/src/App.js
+++ anchor-ui/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/* Internal imports */
+import AnchorInput from './components/anchorinput';
+import Filters from './components/filters';
+import PatientView from './components/patientview';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro text-info">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className='page-header container'>
+					<h1>Anchor Explorer</h1>
+				</div>
+				<div className='App container'>
+					<Filters></Filters>
+					<br />
+					<AnchorInput></AnchorInput>
+					<br />
+					<PatientView></PatientView>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
+
+// REACT BASIC EXAMPLE: http://moviee.surge.sh/
