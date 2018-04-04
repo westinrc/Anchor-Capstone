14edba98d7ad5f734b0c5546ce8a7e0d620298c5
Tori Ottenheimer <vottenhe@asu.edu>
2018-04-04 14:40:25 -0700

    CPST-11 :: Changed UI components to connect with eachother

--- anchor-ui/src/components/cohort.js
+++ anchor-ui/src/components/cohort.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
 import CohortRow from './cohort_row';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import '../css/cohort.css';
 
-class Cohort extends Component {
-	constructor() {
-		super();
-		this.state = {
-			rowTerm: ['car', 'truck', 'tori', 'vehicle', 'westin', 'dhalton', 'richard', 'truck', 'tori', 'vehicle', 'westin', 'dhalton', 'richard']
-		};
-	}
-	
-	render() {
-		return (
-			<div className='panel panel-default'>
+	class Cohort extends Component {
+		constructor() {
+			super();
+			this.state = {
+				rowTerm: [{name: 'car'}, {name: 'truck'}, {name: 'tori'}, {name: 'vehicle'}, {name: 'westin'}, {name: 'dhalton'}, {name: 'richard'}]
+			};
+		}
+
+		render() {
+			const cols=[{dataField: 'name', text:'Cohort Name', title: false}];
+			const selectRow = {
+				mode: 'radio',
+				clickToSelect: true,
+				classes: 'selected',
+				hideSelectColumn: true
+			};
+			return (
+				<div className='panel panel-default'>
 				<div className='panel-heading text-left'>
-					<h3 className='no-margin'>Cohort Selection</h3>
+				<h3 className='no-margin'>Cohort Selection</h3>
 				</div>
-				<div className='panel-body fixed-cohort-body'>
-					<table className='table table-hover'>
-						<tbody>
-							{this.state.rowTerm.map((val, index) => <CohortRow key={index} term={val}></CohortRow>)}
-						</tbody>
-					</table>
+				<div className='panel-body fixed-cohort-body text-left'>
+				<BootstrapTable
+					keyField='name' data={this.state.rowTerm} columns={cols}
+					hover
+					condenced
+					bordered={false}
+					selectRow={selectRow}
+				/>
 				</div>
 				<div className='panel-footer'>
-					<div className='btn-group btn-block'>
-						<button className='btn btn-default'>Remove Cohort</button>
-						<button className='btn btn-default'>Add Cohort</button>	
-					</div>
+				<div className='btn-group btn-block'>
+				<button className='btn btn-default'>Remove Cohort</button>
+				<button className='btn btn-default'>Add Cohort</button>
 				</div>
-			</div>
-		);
-	}
-}
+				</div>
+				</div>
+			);
+		}
 
-export default Cohort;
+	}
+
+	export default Cohort;
